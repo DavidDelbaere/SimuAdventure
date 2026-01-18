e48872803371cbd8fa7d2678dd7d3d2da80c7f7a
Nick Pighin <npighin@gmail.com>
2026-01-18 02:31:42 -0500

    Small change to improve

--- geminiPrompt.py
+++ geminiPrompt.py
@@ -2,28 +2,26 @@ import time
 from dine import fetch
 
 def geminiPrompt(geminiNeededResponse):
-    
-    input()
 
     match geminiNeededResponse:
         case 1:
             #button button
             while(fetch[0] != 1):
-                time.sleep(1)
+                time.sleep(0.5)
                 return(geminiNeededResponse)
             return "I have pressed the button."
         
         case 2:
             #lights button
             while(fetch[1] != 1):
-                time.sleep(1)
+                time.sleep(0.5)
                 return(geminiNeededResponse)
             return "I have turned the lights on."
         
         case 3:
             #heat button
             while(fetch[2] != 1):
-                time.sleep(1)
+                time.sleep(0.5)
                 return(geminiNeededResponse)
             return "I have heated the object up."
         
