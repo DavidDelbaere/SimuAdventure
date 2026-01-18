ed6df3851b0679cb6ccb3cbaf84a51c534ec19b2
SimuOttawa <simuottawa@gmail.com>
2026-01-18 02:40:21 -0500

    son im dine

---
+++ dine.py
@@ -0,0 +1,22 @@
+import socket
+
+UDP_IP = "172.20.10.4" # The Pi's IP address
+UDP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.settimeout(2) # Prevent the script from hanging forever if the Pi is off
+
+def fetch():
+    try:
+        # 1. Send to Pi
+        sock.sendto("Data".encode(), (UDP_IP, UDP_PORT))
+        print(f"Sent to Pi")
+
+        # 2. Wait for Pi to write back
+        data, addr = sock.recvfrom(1024)
+        print(f"Pi replied: {data.decode()}")
+        return data.decode()
+        
+    except socket.timeout:
+        print("Error: Pi did not respond in time.")
+fetch()
